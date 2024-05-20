c98f9c5044477f4f5807cd2452be844b34116ef6
YuvashreeRchan <popsicle2805@gmail.com>
2024-05-20 21:27:15 +0530

    completed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["C:/Users/yuva/Documents/ReactKalvium/lab-react-autocomplete-search-boilerplate"]
+    }
+  }
+});
